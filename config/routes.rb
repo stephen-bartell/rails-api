dff9b171284ece82e30a72e2fef6ab7212c5da05
JP Silvashy <jpsilvashy@gmail.com>
2015-04-24 12:18:58 -0700

    added sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  require "sidekiq/web"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
+
+  mount Sidekiq::Web, at: "/sidekiq"
+
   # constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
