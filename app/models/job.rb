9b6618e10e65219909375fc08102add8fbc7ffd0
JP Silvashy <jpsilvashy@gmail.com>
2015-06-16 07:33:00 -0400

    revise job runner

--- app/models/job.rb
+++ app/models/job.rb
@@ -18,6 +18,7 @@ class Job < ActiveRecord::Base
   after_commit -> { self.queue }
   after_destroy -> { self.unqueue }
 
+  # Jobs need to do something, careful, this gets eval'd
   validates :method_string, presence: true
 
   scope :created,  -> { where(status: 'created') }
@@ -26,17 +27,28 @@ class Job < ActiveRecord::Base
   scope :canceled, -> { where(status: 'canceled') }
   scope :requeued, -> { where(status: 'requeued') }
 
+  # Run the run in method_string, this can be improved, it should
+  # work something like Sidekiq's perform method, where sending in
+  # marshalled objects is discouraged.
   def run
     return if run_count == 0
 
     begin
-      send method_string
-      update_column(:run_count, self.run_count -= 1) if run_count.present?
+      eval method_string
+
+      if run_count.zero?
+        update_column :status, 'finished'
+      else
+        update_column :run_count, self.run_count -= 1
+        update_column :status, 'requeued'
+      end
     rescue
       update_column :status, 'failed'
     end
   end
 
+  # Cancel and unqueue this job, does not delete it. It's not reccomended that
+  # are deleted, canceling provided a better papertrail.
   def cancel!
 
     # Quickly insert a cancel key so that any jobs that may have already
@@ -50,6 +62,8 @@ class Job < ActiveRecord::Base
     update_column :status, 'canceled'
   end
 
+  # parses the crontab-like attribute `reoccurrence_crontab` and gets the next
+  # time that this job should run.
   def next_run_at
     cron = CronParser.new(reoccurrence_crontab)
     run_at = cron.next(Time.now)
@@ -69,7 +83,6 @@ class Job < ActiveRecord::Base
     # Since this runs `after_commit` now, we need to unqueue and requeue
     # the job anytime it's touched.
     unqueue
-    # cancel!
 
     if run_at && run_at > Time.now
       jid = ScheduleJobWorker.perform_at(next_run_at, id, method_string)
--- db/migrate/20150601175336_create_jobs.rb
+++ db/migrate/20150601175336_create_jobs.rb
@@ -2,10 +2,10 @@ class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
       t.string :jid, index: true
-      t.string :method_string
+      t.string :method_string, null: false
       t.string :reoccurrence_crontab
       t.datetime :run_at
-      t.integer :run_count
+      t.integer :run_count, default: 1
       t.string :status
 
       t.uuid :resource_id
